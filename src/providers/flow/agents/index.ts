a6c0bc667ecf647068cd333edfd499deee533f42
eltonjr13 <eltonjr_bn@hotmail.com>
2026-07-24 15:19:34 -0300

    FlowAgent facade for task orchestration via Scheduler and registered agents

---
+++ src/providers/flow/agents/index.ts
@@ -0,0 +1,6 @@
+export * from "./CreativeAgent";
+export * from "./FlowAgentContracts";
+export * from "./ImageAgent";
+export * from "./ProjectAgent";
+export * from "./RefineAgent";
+export * from "./VideoAgent";
